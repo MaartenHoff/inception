9a75fee2688762b93db365c64bcd9740d290b9f2
maarten <maarten.berlin@gmail.com>
2025-11-07 15:54:07 +0000

    wordpress works!

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,8 +1,19 @@
 <?php
-define( 'DB_NAME', getenv('thedatabase') );
-define( 'DB_USER', getenv('theuser') );
-define( 'DB_PASSWORD', getenv('abc') );
-define( 'DB_HOST', getenv('mariadb') );
-define( 'WP_HOME', getenv('https://login.42.fr') );
-define( 'WP_SITEURL', getenv('https://login.42.fr') );
+define( 'DB_NAME', "thedatabase");
+define( 'DB_USER', "theuser");
+define( 'DB_PASSWORD', "abc" );
+define( 'DB_HOST', "mariadb" );
+define( 'WP_HOME', "https://login.42.fr" );
+define( 'WP_SITEURL', "https://login.42.fr" );
+
+$table_prefix = 'wp_';
+
+define( 'WP_DEBUG', true );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+require_once ABSPATH . 'wp-settings.php';
+
 ?>
