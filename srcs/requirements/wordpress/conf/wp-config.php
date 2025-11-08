0eac6129b47a5e93d4779097fe83e33d0378f62e
maarten <maarten.berlin@gmail.com>
2025-11-08 16:09:00 +0000

    some more env var changes

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -1,10 +1,10 @@
 <?php
-define( 'DB_NAME', "thedatabase");
-define( 'DB_USER', "theuser");
-define( 'DB_PASSWORD', "abc" );
-define( 'DB_HOST', "mariadb" );
-define( 'WP_HOME', "https://login.42.fr" );
-define( 'WP_SITEURL', "https://login.42.fr" );
+define( 'DB_NAME', getenv('DB_NAME'));
+define( 'DB_USER', getenv('DB_USER'));
+define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
+define( 'DB_HOST', getenv('DB_HOST') );
+define( 'WP_HOME', getenv('WP_FULL_URL') );
+define( 'WP_SITEURL', getenv('WP_FULL_URL') );
 
 $table_prefix = 'wp_';
 
